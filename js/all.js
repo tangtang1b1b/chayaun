80512f54751adc56e1b063c36247a6bb93f13fae
tangtang1b1b <a0981632653@gmail.com>
2022-09-26 18:19:34 +0800

    總算

--- js/all.js
+++ js/all.js
@@ -15,9 +15,9 @@ $(window).on("scroll",function(e){
 $(window).on("mousemove",function(e){ 
     let pageX = e.pageX;
     let pageY = e.pageY;
-    let x = pageX-$("#special_group").offset().left;
-    let y = pageY-$("#special_group").offset().top;
-    if(y<0 || x<0 || y>$("#special_group").outerHeight() || x>$("#special_group").outerHeight()){
+    let x = pageX-$("#special_group .derivative_list").offset().left;
+    let y = pageY-$("#special_group .derivative_list").offset().top;
+    if(y<0 || x<0 || y>$("#special_group").outerHeight() || x>$("#special_group").outerWidth()){
         $("#followbtn").css("opacity","0");
     }else{
         $("#followbtn").css("opacity","1");
