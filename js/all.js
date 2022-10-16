294f7d42d79ae0950fab602f9041a1eca9199c89
tangtang1b1b <a0981632653@gmail.com>
2022-10-17 02:09:49 +0800

    e04

--- js/all.js
+++ js/all.js
@@ -78,6 +78,46 @@ $(".hamber").click(function(){
     }
 });
 
+let check = true;
+let check2 = true;
+$(".all_type").click(function(){
+    if(check && check2){
+        $(".choose_group").slideDown();
+        $("#gogogo").addClass("back");
+        check =false;
+    }else if(!check && check2){
+        $(".choose_group").slideUp();
+        $("#gogogo").removeClass("back");
+        check = true;
+    }
+});
+$(".sert_big_clone").click(function(){
+    if(check2 && check){
+        $(".sert_group_clone").slideDown();
+        $("#popogo").addClass("back");
+        check2 =false;
+    }else if(!check2 && check){
+        $(".sert_group_clone").slideUp();
+        $("#popogo").removeClass("back");
+        check2 = true;
+    }
+});
+
+let sect = $(".product_menu");
+let prod = $(".type_list .a");
+// console.log($(".product_menu"));
+$(prod[0]).addClass("bc");
+$(".type_list").click(function(){
+    $(this).attr("data-num");
+    i = parseInt($(this).attr("data-num"));
+    for(let i =0;i<sect.length;i++){
+        $(prod[i]).removeClass("bc");
+        $(sect[i]).css("display","none");
+    }
+    $(sect[i]).css("display","block");
+    $(prod[i]).addClass("bc");
+});
+
 // 重製區
 $(window).resize(function() {
     if($(window).width()>1024){
