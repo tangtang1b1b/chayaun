1584281a58b581c04e431cd0b0bfd3dbf38dd425
tangtang1b1b <a0981632653@gmail.com>
2022-10-16 20:23:25 +0800

    hhh

--- js/all.js
+++ js/all.js
@@ -83,18 +83,22 @@ let check2 = true;
 $(".all_type").click(function(){
     if(check && check2){
         $(".choose_group").slideDown();
+        $("#gogogo").addClass("back");
         check =false;
     }else if(!check && check2){
         $(".choose_group").slideUp();
+        $("#gogogo").removeClass("back");
         check = true;
     }
 });
 $(".sert_big_clone").click(function(){
     if(check2 && check){
         $(".sert_group_clone").slideDown();
+        $("#popogo").addClass("back");
         check2 =false;
     }else if(!check2 && check){
         $(".sert_group_clone").slideUp();
+        $("#popogo").removeClass("back");
         check2 = true;
     }
 });
