c830aae2b575e9eb24b4f5253e7741922566d1c4
tangtang1b1b <a0981632653@gmail.com>
2022-10-19 01:31:48 +0800

    e04

--- js/all.js
+++ js/all.js
@@ -137,7 +137,31 @@ $(".type_list").click(function(){
 //     }
 //   });
   
-
+let oth = $(window).innerHeight();
+$(window).scroll(function(){
+    let sct = $(this).scrollTop();
+    let all = sct+oth;
+    let oft = $("#special_group").offset().top;
+    if(all > oft){
+        $("#special_group").animate({left:"40%"},1200);
+    }
+    // const els = document.querySelectorAll(".slideIn");
+    // console.log(els)
+    // els.forEach((v,x) => {
+    //     let elTop = v.getBoundingClientRect().top;
+    //     if (elTop < window.innerHeight && elTop> 0) {
+    //         console.log('visible',x)
+    //     } else {
+    //         console.log('invisibe',x)
+    //     }
+    // })
+    $(".slideIn").each(function(){
+        let sld = $(this).offset().top;
+        if(all > sld){
+            $(this).addClass("active");
+        }
+    });
+});
 // 重製區
 $(window).resize(function() {
     if($(window).width()>1024){
