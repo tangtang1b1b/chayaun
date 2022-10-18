ab31242aa2634426e1c12ec49b2349de78003ccd
tangtang1b1b <a0981632653@gmail.com>
2022-10-19 00:15:24 +0800

    123

--- js/scroll.js
+++ js/scroll.js
@@ -6,7 +6,7 @@
         let all = sct+oth;
         let oft = $("#special_group").offset().top;
         if(all > oft){
-            $("#special_group").animate({left:"40%"},1000);
+            $("#special_group").animate({left:"40%"},1100);
         }
         // const els = document.querySelectorAll(".slideIn");
         // console.log(els)
@@ -22,10 +22,8 @@
             let sld = $(this).offset().top;
             if(all > sld){
                 $(this).addClass("active");
-            }else{
-                // $(this).addClass("active");
             }
-            // console.log(all)
+            console.log($(".slideIn"))
         });
     });
 // }
