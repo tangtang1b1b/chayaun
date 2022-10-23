5b8632b006d9cb583bddd8f92577d786ec5b3038
tangtang1b1b <a0981632653@gmail.com>
2022-10-23 11:08:41 +0800

    t

--- js/addcar.js
+++ js/addcar.js
@@ -1,7 +1,6 @@
 let shopcar = document.querySelector(".shopcar");
 let itemlist = document.querySelector("#productcontent_group");
-let newfuck = JSON.parse(localStorage.getItem("fuck"));
-// let del = document.querySelectorAll(".del");
+// let newfuck = JSON.parse(localStorage.getItem("fuck"));
 // let gg = JSON.parse(localStorage.getItem(fuck));
 // let fuck = ["img/22.jpg","850","850"];
 // let fuck = [{picuuuu:"img/22.jpg"},{priceee:"850"},{piccc:"850"}];
@@ -21,23 +20,22 @@ $(".shopcar").click(function(){
     showla();
     // console.log($(this).data("num"));
 });
-$(".del").click(function(){
-    fuck.splice(0,1);
-    localStorage.setItem("fuck", JSON.stringify(fuck));
-    showla();
-});
-console.log(fuck)
 
-let myclass = `<li class='productcontent'><div class='productname'><img src={img}></div><div class='productname proprice'>{price}</div><div id='math' class='productname'><div class='minus'>-</div><div class='number'>1</div><div class='plus'>+</div></div><div class='productname producttotal'>{total}</div><div class='productname del'>x</div></li>`;
+let myclass = `<li class='productcontent' data-del={del}><div class='productname'><img src={img}></div><div class='productname proprice'>{price}</div><div id='math' class='productname'><div class='minus'>-</div><div class='number'>1</div><div class='plus'>+</div></div><div class='productname producttotal'>{total}</div><div class='productname del'>x</div></li>`;
+
 
 function showla(){
-    for(let i = 0;i<newfuck.length;i++){
-        hahadead = newfuck[i];
-
+    for(let i = 0;i<fuck.length;i++){
+        newfuck = fuck[i];
+        // $("li").click(function(){
+        //     fuck = newfuck.splice(0,1);
+        //     localStorage.setItem("fuck", JSON.stringify(fuck));
+        // });
     
-        let myclassnew = myclass.replace("{img}",hahadead.picuuuu)
-                                .replace("{price}",hahadead.priceee)
-                                .replace("{total}",hahadead.piccc)
+        let myclassnew = myclass.replace("{img}",newfuck.picuuuu)
+                                .replace("{price}",newfuck.priceee)
+                                .replace("{total}",newfuck.piccc)
+                                .replace("{del}",i)
                                 
         $(itemlist).append(myclassnew);
         // console.log(newfuck)
@@ -45,3 +43,11 @@ function showla(){
 }
 // console.log(JSON.parse(localStorage.getItem("fuck")))
 showla();
+// $(".del").click(function(){
+//     dele($(this).data("del"));
+// });
+// function dele(i){
+//     fuck.splice(i,1);
+//     showla();
+// }
+console.log(fuck);
